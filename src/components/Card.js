29ead8cd9f9b1ab21b86cf4c6f5eacf5c51f5041
Chrissus14 <cjgm14@gmail.com>
2020-06-13 19:27:42 -0500

    card-list for desktop

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,7 @@ export default function Card({ movie }) {
       <img className="card__image" src={movie.Poster} alt={movie.Title} />
       <div className="card__content">
         <h3 className="card__title">{movie.Title}</h3>
+        <p><span>{movie.Type}</span></p>
         <p><small>AÑO DEL ESTRENO: {movie.Year}</small></p>
       </div>
     </div>
